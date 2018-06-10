1339248a78fb791141a46564e9fcd461713a7d8a
dougfort <doug.fort@gmail.com>
2018-06-10 07:31:21 -0400

    clean out initial proto file

--- 02/go_out.sh
+++ 02/go_out.sh
@@ -4,8 +4,7 @@ set -euxo pipefail
 
 MYSTOREPATH="$GOPATH/src/github.com/dougfort/mystore/02"
 
-protoc -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-    --proto_path=$MYSTOREPATH/protobuf \
+protoc --proto_path=$MYSTOREPATH/protobuf \
     --plugin=$GOPATH/bin/protoc-gen-go \
     --go_out=plugins=grpc:$MYSTOREPATH/protobuf \
     $MYSTOREPATH/protobuf/mystore.proto
